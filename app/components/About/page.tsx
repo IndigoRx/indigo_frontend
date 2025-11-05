e2b7aa48aec981babe6cf8d861a4a31f8a919c96
Sreekuttan M B <96076573+sksree@users.noreply.github.com>
2025-11-05 23:24:12 +0530

    Login page

---
+++ app/components/About/page.tsx
@@ -0,0 +1,9 @@
+import Navbar from "../Navbar/page";
+
+
+export default function About(){
+    return <>
+    <Navbar />
+    <h1>This is About page</h1>
+    </>
+}
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "http://147.93.114.66:9090/:path*", // Proxy to backend
+      },
+    ];
+  },
 };
-
 export default nextConfig;
