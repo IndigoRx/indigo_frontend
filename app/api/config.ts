d08c2fd5612cc73437f535c2c0c969b3c3ae41e7
zeezaglobal <96932409+zeezaglobal@users.noreply.github.com>
2025-12-07 20:52:15 -0600

    bug fix

--- app/api/config.ts
+++ app/api/config.ts
@@ -1,5 +1,5 @@
-//export const API_BASE_URL = "http://147.93.114.66:9090";
-export const API_BASE_URL = "http://localhost:9090";
+export const API_BASE_URL = "http://147.93.114.66:9090";
+//export const API_BASE_URL = "http://localhost:9090";
 export const API_ENDPOINTS = {
   LOGIN: `${API_BASE_URL}/auth/login`,
   REGISTER: `${API_BASE_URL}/auth/register`,
--- app/components/About/page.tsx
+++ app/components/About/page.tsx
@@ -3,11 +3,8 @@ import Image from "next/image";
 import { useEffect, useState } from "react";
 import { Roboto } from "next/font/google";
 import Navbar from "../Navbar/page";
-import ImageCard from "../SubComponents/HomeCard";
-const roboto = Roboto({
-  weight: ["400", "500"],
-  subsets: ["latin"],
-});
+
+
 
 
 export default function About(){
