46d289158a353d2ad7d92fe1a49120b52fe2581e
zeezaglobal <96932409+zeezaglobal@users.noreply.github.com>
2025-12-14 21:13:13 -0600

    url fix

--- app/api/config.ts
+++ app/api/config.ts
@@ -1,4 +1,4 @@
-export const API_BASE_URL = "http://147.93.114.66:9090";
+export const API_BASE_URL = "https://api.indigorx.me";
 //export const API_BASE_URL = "http://localhost:9090";
 export const API_ENDPOINTS = {
   LOGIN: `${API_BASE_URL}/auth/login`,
