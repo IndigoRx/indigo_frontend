d433e48eb1c2224970765161aced06f502f389eb
zeezaglobal <96932409+zeezaglobal@users.noreply.github.com>
2026-01-11 23:29:03 -0600

    bug fix

--- next.config.ts
+++ next.config.ts
@@ -18,11 +18,11 @@ const nextConfig = {
             key: 'Content-Security-Policy',
             value: [
               "default-src 'self'",
-              "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://js.stripe.com",
+              "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://js.stripe.com https://static.cloudflareinsights.com",
               "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
               "font-src 'self' https://fonts.gstatic.com",
               "img-src 'self' data: https: blob:",
-              "connect-src 'self' http://147.93.114.66:9090 https://api.stripe.com",
+              "connect-src 'self' https://api.indigorx.me http://147.93.114.66:9090 https://api.stripe.com https://cloudflareinsights.com",
               "frame-src 'self' https://js.stripe.com https://hooks.stripe.com",
               "frame-ancestors 'none'",
               "form-action 'self'",
