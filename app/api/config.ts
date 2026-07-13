87f9cb0153e7b1bb8bff1190fdca9cea5d43bbd6
zeezaglobal <96932409+zeezaglobal@users.noreply.github.com>
2026-07-12 23:56:22 -0500

    sdrg

--- app/api/config.ts
+++ app/api/config.ts
@@ -1,5 +1,5 @@
-//export const API_BASE_URL = "https://api.indigorx.me";
-export const API_BASE_URL = "http://localhost:9090";
+export const API_BASE_URL = "https://api.indigorx.me";
+//export const API_BASE_URL = "http://localhost:9090";
 
 export const API_ENDPOINTS = {
   BASE_URL: API_BASE_URL,
